f084e08041b9abd803d360683d116bf02958e1d9
Mazri02 <azri.mokhzani@gmail.com>
2024-12-04 16:48:53 +0800

    Change files and keep necessary file only

--- resources/js/Pages/Cart.tsx
+++ resources/js/Pages/Cart.tsx
@@ -3,13 +3,11 @@ import { Head } from '@inertiajs/react';
 export default function Dashboard() {
     return (
         <div>
-            <Head title="Dashboard" />
-
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            Test New Routing File
                         </div>
                     </div>
                 </div>
